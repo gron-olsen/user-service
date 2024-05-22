1501ce9454626a059cbd2249671e1b750d44f208
MagnusHeesch <magnusheesch@live.dk>
2024-05-22 19:02:59 +0200

    cleanup

--- user-serviceAPI/Services/UserRepository.cs
+++ user-serviceAPI/Services/UserRepository.cs
@@ -2,54 +2,67 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using userServiceAPI.Models;
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Misc;
 using userServiceAPI.Services;
 
-namespace userServiceAPI.Services {
-
-public class UserRepository : IUserRepository
+namespace userServiceAPI.Services
 {
-    public readonly IConfiguration _configuration;
-    private readonly IMongoCollection<User> _userCollection;
 
-    public UserRepository(ILogger<UserRepository> logger, IConfiguration configuration)
+    public class UserRepository : IUserRepository
     {
-        
-        _configuration = configuration;
-    
-        logger.LogInformation($"INFO: connection string is: {configuration["connectionString"]}");
-    
-        var mongoClient = new MongoClient(_configuration["connectionString"]);
-        var database = mongoClient.GetDatabase(_configuration["database"]);
-        _userCollection = database.GetCollection<User>(_configuration["collection"]);
+        public readonly IConfiguration _configuration;
+        private readonly IMongoCollection<User> _userCollection;
+
+        public readonly ILogger<UserRepository> _logger;
+
+
+        public UserRepository(ILogger<UserRepository> logger, IConfiguration configuration)
+        {
+
+            _configuration = configuration;
+            _logger = logger;
+
+            _logger.LogInformation($"INFO: connection string is: {_configuration["connectionString"]}");
+
+            try
+            {
+                var mongoClient = new MongoClient(_configuration["connectionString"]);
+                var database = mongoClient.GetDatabase(_configuration["database"]);
+                _userCollection = database.GetCollection<User>(_configuration["collection"]);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred while connecting to MongoDB: {ex.Message}");
+                throw;
+            }
+        }
+
+            public async Task AddUser(User user) =>
+                await _userCollection.InsertOneAsync(user);
+
+
+            public async Task<User> GetUser(LoginModel loginModel) =>
+
+                await _userCollection.Find(u => u.UserName == loginModel.UserName).FirstOrDefaultAsync();
+
+            public async Task<User> GetUserById(int id) =>
+
+                 await _userCollection.Find(u => u.UserID == id).FirstOrDefaultAsync();
+
+
+            public async Task<List<User>> GetAllUsers() =>
+
+                await _userCollection.Find(_ => true).ToListAsync();
+
+
+
+            public async Task UpdateUser(int id, User user) =>
+
+                await _userCollection.ReplaceOneAsync(u => u.UserID == id, user);
+
+
+            public async Task DeleteUser(int id) =>
+
+                await _userCollection.DeleteOneAsync(u => u.UserID == id);
+        }
     }
-    
-
-    public async Task AddUser(User user) =>
-        await _userCollection.InsertOneAsync(user);
-
-    
-    public async Task<User> GetUser(LoginModel loginModel) =>
-
-        await _userCollection.Find(u => u.UserName == loginModel.UserName).FirstOrDefaultAsync();
-
-   public async Task<User> GetUserById(int id) =>
-
-        await _userCollection.Find(u => u.UserID == id).FirstOrDefaultAsync();
-
-    
-    public async Task<List<User>> GetAllUsers() =>
-
-        await _userCollection.Find(_ => true).ToListAsync();
-    
-
-
-    public async Task UpdateUser(int id, User user) =>
-
-        await _userCollection.ReplaceOneAsync(u => u.UserID == id, user);
-
-
-    public async Task DeleteUser(int id) =>
-    
-        await _userCollection.DeleteOneAsync(u => u.UserID == id);
-}
-}
